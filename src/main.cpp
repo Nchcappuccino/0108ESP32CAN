94879532b4a7520b23a91859dc5186b49803a45a
Yudai-laptop <yudai.yy0804@gmail.com>
2021-01-11 11:49:37 +0900

    CANのピン番号を直した

--- src/main.cpp
+++ src/main.cpp
@@ -9,40 +9,26 @@ int i;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("iotsharing.com CAN demo");
+  Serial.println("CAN start");
   CAN_cfg.speed=CAN_SPEED_1000KBPS;
-  CAN_cfg.tx_pin_id = GPIO_NUM_5;
-  CAN_cfg.rx_pin_id = GPIO_NUM_4;
+  // CAN_cfg.speed=CAN_SPEED_500KBPS;
+  CAN_cfg.tx_pin_id = GPIO_NUM_4;
+  CAN_cfg.rx_pin_id = GPIO_NUM_2;
   CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
   //start CAN Module
   ESP32Can.CANInit();
 }
 
 void loop() {
+  // CAN_frame_t tx_frame;
+  Serial.printf("a\t");
   // put your main code here, to run repeatedly:
-  if(i<250){
-    i++;
-  }else{
-    i=0;
-  }
-  tx_frame.FIR.B.FF = CAN_frame_std;
-  tx_frame.MsgID = 1110;
-  tx_frame.FIR.B.DLC = 8;
-  tx_frame.data.u8[0] = i;
-  tx_frame.data.u8[1] = 100;
-  tx_frame.data.u8[2] = 200;
-  tx_frame.data.u8[3] = 0;
-  tx_frame.data.u8[4] = 0;
-  tx_frame.data.u8[5] = 0;
-  tx_frame.data.u8[6] = 0;
-  tx_frame.data.u8[7] = 0;
-  tx_frame.data.u8[8] = 0;
-
-  ESP32Can.CANWriteFrame(&tx_frame);
+/*
   
       if(xQueueReceive(CAN_cfg.rx_queue,&rx_frame, 3*portTICK_PERIOD_MS)==pdTRUE){
-
+        Serial.printf("b\t");
       //do stuff!
+
       if(rx_frame.FIR.B.FF==CAN_frame_std)
         printf("New standard frame");
       else
@@ -58,5 +44,25 @@ void loop() {
         printf("\n");
       }
     }
-
+  */
+  if(i<250){
+    i++;
+  }else{
+    i=0;
+  }
+  tx_frame.FIR.B.FF = CAN_frame_std;
+  tx_frame.MsgID = 1110;
+  // tx_frame.MsgID = 1;
+  tx_frame.FIR.B.DLC = 8;
+  tx_frame.data.u8[0] = 0;
+  tx_frame.data.u8[1] = 100;
+  tx_frame.data.u8[2] = i;
+  tx_frame.data.u8[3] = 0;
+  tx_frame.data.u8[4] = 0;
+  tx_frame.data.u8[5] = 0;
+  tx_frame.data.u8[6] = 0;
+  tx_frame.data.u8[7] = 0;
+  ESP32Can.CANWriteFrame(&tx_frame);
+  delay(1);
+  Serial.printf("\r\n");
 }
